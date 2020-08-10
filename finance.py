29506c5efe92da3795f28a7631e31ab881e19fdf
ubwr <50961790+ubwr@users.noreply.github.com>
2020-08-09 23:02:08 -0500

    Add files via upload

--- finance.py
+++ finance.py
@@ -6,31 +6,66 @@ from pandas.tseries.offsets import CustomBusinessMonthBegin
 import pandas_datareader.data as web
 from pandas import DataFrame
 import numpy as np
+import os
 
 
 today = dt.datetime.today()
 style.use('ggplot')
-start = dt.datetime(2019, 8, 7)
 
-tick = 'AAPL'
+tick = 'TSLA'
 
+start = dt.datetime(2018, 12, 1)
+end = dt.datetime(2019, start.month, start.day)
+
+##snp_oh_tickers_o = ['AAPL', 'ABBV', 'ABT', 'ACN', 'ADBE', 'AIG', 'ALL', 'AMGN', 'AMT', 'AMZN', 'AXP', 'BA', 'BAC', 'BIIB', 'BK',
+##                  'BKNG', 'BLK', 'BMY', 'BRK-B', 'C']
+##snp_oh_tickers_t = ['CAT', 'CHTR', 'CL', 'CMCSA', 'COF', 'COP', 'COST', 'CRM', 'CSCO', 'CVS', 'CVX', 'DD', 'DHR', 'DIS', 'DOW',
+##                    'DUK', 'EMR', 'EXC', 'F', 'FB']
+##snp_oh_tickers_th = ['FDX', 'GD', 'GE', 'GILD', 'GM', 'GOOG', 'GOOGL', 'GS', 'HD', 'HON', 'IBM', 'INTC', 'JNJ', 'JPM', 'KHC',
+##                     'KMI', 'KO', 'LLY', 'LMT', 'LOW']
+##snp_oh_tickers_f = ['MA', 'MCD', 'MDLZ', 'MDT', 'MET', 'MMM', 'MO', 'MRK', 'MS', 'MSFT', 'NEE', 'NFLX', 'NKE', 'NVDA', 'ORCL', 'OXY', 'PEP',
+##                  'PFE', 'PG', 'PM']
+##snp_oh_tickers_fv = ['PYPL', 'QCOM', 'RTX', 'SBUX', 'SLB', 'SO', 'SPG', 'T', 'TGT', 'TMO', 'TXN', 'UNH', 'UNP',
+##                  'UPS', 'USB', 'V', 'VZ', 'WBA', 'WFC', 'WMT']
+##snp_oh_tickers_s = ['XOM']
+
+#snp excluding: 'AIG', 'CRM', 'CSCO', 'GOOGL', 'HD', 'MDLZ', 'MET', 'MS', 'NEE', 'NFLX', 'RTX', 'SPG', 'TMO',
+# 'UNH', 'VZ', = bad data
+
+snp_oh_tickers = ['AAPL', 'ABBV', 'ABT', 'ACN', 'ADBE', 'ALL', 'AMGN', 'AMT', 'AMZN', 'AXP', 'BA', 'BAC', 'BIIB', 'BK',
+                  'BKNG', 'BLK', 'BMY', 'BRK-B', 'C', 'CAT', 'CHTR', 'CL', 'CMCSA', 'COF', 'COP', 'COST', 'CVS', 'CVX', 'DD', 'DHR', 'DIS', 'DOW',
+                    'DUK', 'EMR', 'EXC', 'F', 'FB', 'FDX', 'GD', 'GE', 'GILD', 'GM', 'GOOG', 'GS', 'HON', 'IBM', 'INTC', 'JNJ', 'JPM', 'KHC',
+                     'KMI', 'KO', 'LLY', 'LMT', 'LOW', 'MA', 'MCD', 'MDT', 'MMM', 'MO', 'MRK', 'MSFT', 'NKE', 'NVDA', 'ORCL', 'OXY', 'PEP',
+                  'PFE', 'PG', 'PM', 'PYPL', 'QCOM', 'SBUX', 'SLB', 'SO', 'T', 'TGT', 'TXN', 'UNP',
+                  'UPS', 'USB', 'V', 'WBA', 'WFC', 'WMT', 'XOM']
+
+
+
+#print(len(snp_oh_tickers))
 #end = dt.datetime(today.year, today.month, today.day)
-end = dt.datetime(2020, 8, 7)
+start = dt.datetime(2018, 1, 2)
+end = dt.datetime(2019, start.month, start.day)
 #end = dt.datetime(today.year, today.month, today.day)
+
 #end = dt.datetime(start.year+1, start.month, start.day)
 
 #print('Start date: ' + str(start))
 #print('End date: ' + str(end))
 
 
+
 def calculate_daily(ticker, day_of_week):
     df = web.DataReader(ticker, 'yahoo', start, end)
+    
     df.to_csv('Data')
     
     df = pd.read_csv('Data')
 
+    print(df)
+
     df['Date'] = pd.to_datetime(df['Date'])
     df['Day of Week'] = df['Date'].dt.day_name()
+
 
     #daily_df = DataFrame({'% chance of closing up in daily time frame':[]})
 
@@ -40,11 +75,19 @@ def calculate_daily(ticker, day_of_week):
     closer_higher_than_open_df = day_of_df['Close'] > day_of_df['Open']
     higher_df = day_of_df[closer_higher_than_open_df]
 
+    #print(higher_df)
+
     percent_change = 0
 
     for row in range(0, len(higher_df)):
         percent_change += ((higher_df.iloc[row]['Close'] / higher_df.iloc[row]['Open'])-1)*100
 
+    print(ticker)
+    print(percent_change)
+    print(len(higher_df))
+##    if (len(higher_df)) == 0:
+##        avg_percent_change = 0
+##    else:
     avg_percent_change = percent_change / len(higher_df)
 
 ##    print(ticker + ' ^ ' + day_of_week + ' | ' + str(round(((len(higher_df)/len(day_of_df))*100), 2))+'%'+' |'
@@ -58,11 +101,6 @@ def calculate_daily(ticker, day_of_week):
 ##    start.year += 1
 
 #calculate_daily()
-
-daily_df = DataFrame({'Day of the Week':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
-                      'Percent': [calculate_daily(tick, 'Monday')[0], calculate_daily(tick, 'Tuesday')[0], calculate_daily(tick, 'Wednesday')[0], calculate_daily(tick, 'Thursday')[0], calculate_daily(tick, 'Friday')[0]],
-                      'Average Change': [calculate_daily(tick, 'Monday')[1], calculate_daily(tick, 'Tuesday')[1], calculate_daily(tick, 'Wednesday')[1], calculate_daily(tick, 'Thursday')[1], calculate_daily(tick, 'Friday')[1]]})
-
 ##ax = daily_df.plot.bar(x='Day of the Week', y='Percent', rot=0)
 ##
 ##ax.set_title('Chance of closing up per weekday from ' + str(start.year) + '-' + str(start.month) + '-' + str(start.day)
@@ -71,6 +109,13 @@ daily_df = DataFrame({'Day of the Week':['Monday', 'Tuesday', 'Wednesday', 'Thur
 ##plt.show()
 
 ########################################################################
+
+#for tick in snp_oh_tickers[80:100]:
+
+#for day_timer in range (1, 33):
+daily_df = DataFrame({'Day of the Week':['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+                      'Percent': [calculate_daily(tick, 'Monday')[0], calculate_daily(tick, 'Tuesday')[0], calculate_daily(tick, 'Wednesday')[0], calculate_daily(tick, 'Thursday')[0], calculate_daily(tick, 'Friday')[0]],
+                      'Average Change': [calculate_daily(tick, 'Monday')[1], calculate_daily(tick, 'Tuesday')[1], calculate_daily(tick, 'Wednesday')[1], calculate_daily(tick, 'Thursday')[1], calculate_daily(tick, 'Friday')[1]]})
 
 indx = np.arange(len(daily_df['Percent']))
 percent_label = np.arange(0, 110, 10)
@@ -88,20 +133,10 @@ ax.legend()
 ax.set_title(tick + ' % chance of closing up per weekday from ' + str(start.year) + '-' + str(start.month) + '-' + str(start.day)
              + ' to ' + str(end.year) + '-' + str(end.month) + '-' + str(end.day))
 
-##print(0%2)
-##print(1%2)
-##print(2%2)
-##print(3%2)
 
 def insert_data_labels(bars):
-##    temp = 0
     for bar in bars:
             bar_height = bar.get_height()
-##            if temp % 2 == 0:
-##                bar.set_color('g')
-##            else:
-##                bar.set_color('b')
-##            temp += 1
             ax.annotate(str('{:.2f}'.format(bar.get_height()) + '%'),
                 xy=(bar.get_x() + bar.get_width() / 2, bar_height),
                 xytext=(0, 3),
@@ -113,6 +148,9 @@ insert_data_labels(percent_bar)
 insert_data_labels(avg_change_bar)
 
 plt.show()
+
+fig.set_size_inches(12,9)
+#fig.savefig('TSLA Timelapse/'+ str(start.year) + '-' + str(start.month) + '-' + str(start.day)+'.png')
 
 ########################################################################
 
